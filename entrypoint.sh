818e402338f32052708ce77b43f395c175555959
Forest10 <github.forest10@gmail.com>
2021-04-18 17:20:25 +0800

    name and email

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,24 +16,17 @@ UDX_DB_GIT_DIR=$(pwd)
 git checkout udx-db
 git pull
 UDX_DB_HOME=/home/runner/work/_temp/_github_home/.upx.db
-if [ -d ${UDX_DB_HOME} ];then
-  echo "文件夹存在"
-  # shellcheck disable=SC2035
-  cp -r * ${UDX_DB_HOME}
-  else
-  echo "文件夹不存在,新建"
-  mkdir -p ${UDX_DB_HOME}
-  cp -r "${UDX_DB_GIT_DIR}" ${UDX_DB_HOME}
-  cd  ${UDX_DB_HOME} && ls
-fi
-
+mkdir -p ${UDX_DB_HOME}
+cd ${UDX_DB_HOME}
+cp -r /${UDX_DB_GIT_DIR} /${UDX_DB_HOME}
+cd  ${UDX_DB_HOME} && ls
 
 cd $GITHUB_WORKSPACE/upx-dir/upx-command-dir
 ./upx login ${BUCKET_NAME} ${UPX_NAME} ${UPX_PASSWORD}
 ./upx info
 ./upx sync $GITHUB_WORKSPACE/public / -v
 ## add upx.db to upx.db git
-cp -r ${UDX_DB_HOME} ${UDX_DB_GIT_DIR}
+cp -r /${UDX_DB_HOME} /${UDX_DB_GIT_DIR}
 cd ${UDX_DB_GIT_DIR}
 echo $(date) >date.txt
 git add -A
