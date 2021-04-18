67188f0781df7b4237eada2566cc9de042aeb595
Forest10 <github.forest10@gmail.com>
2021-04-18 15:52:01 +0800

    echo 'Start push' git push
    echo "Deployment to git succesful!"

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,33 @@ cd $GITHUB_WORKSPACE
 ./node_modules/hexo/bin/hexo clean
 #echo "Generate file ..."
 ./node_modules/hexo/bin/hexo generate
+###判断.upx.db存在?
+git clone git@github.com:Forest10/my-hexo-conf.git udx-db
+cd udx-db
+UDX_DB_GIT_DIR=$(pwd)
+git checkout udx-db
+git pull
+UDX_DB_HOME=/home/runner/work/_temp/_github_home/.upx.db
+if [ -d ${UDX_DB_HOME} ];then
+  echo "文件夹存在"
+  cp -r * ${UDX_DB_HOME}
+fi
+
 
 cd upx-dir/upx-command-dir
 ./upx login ${BUCKET_NAME} ${UPX_NAME} ${UPX_PASSWORD}
 ./upx info
 ./upx sync $GITHUB_WORKSPACE/public / -v
+## add upx.db to upx.db git
+cp -r ${UDX_DB_HOME} ${UDX_DB_GIT_DIR}
+cd ${UDX_DB_GIT_DIR}
+echo $(date) >date.txt
+git add -A
+git commit -m '哈哈'
+echo 'Start push'
+git push
+
+echo "push to udx-db-git successful!"
+
+
+
