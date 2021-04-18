e431a3f5f0e055cb815ddcd6cddf30cc90cb75dd
Forest10 <github.forest10@gmail.com>
2021-04-18 21:17:48 +0800

    #### 用git diff搞定差异文件

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,8 @@ cd $GITHUB_WORKSPACE
 ./node_modules/hexo/bin/hexo clean
 #echo "Generate file ..."
 ./node_modules/hexo/bin/hexo generate
+
+#### 用git diff搞定差异文件
 cd /github/home
 git config --global user.email "github.forest10@gmail.com"
 git config --global user.name "Forest10"
@@ -23,22 +25,21 @@ cd udx-db
 git checkout udx-db
 git pull
 cp -r $GITHUB_WORKSPACE/public/* /github/home/udx-db
-echo $(date) >date.txt
 git add -A
-git commit -m '哈哈'
-echo 'Start push'
+git commit -m 'push by hexo-deploy-upyun-action'
 git push
 echo "push to udx-db-git successful!"
 UPDATE_BLOG_FILE=$GITHUB_WORKSPACE/public/update_blog_file
 mkdir -p $UPDATE_BLOG_FILE
 git diff  HEAD^ HEAD --name-only >> diff.txt
-cat diff.txt
 for i in $(cat diff.txt); do cp -r /github/home/udx-db/${i} ${UPDATE_BLOG_FILE}; done
-cd ${UPDATE_BLOG_FILE} && ls
+### 执行upx upload
 cd $GITHUB_WORKSPACE/upx-dir/upx-command-dir
 ./upx login ${BUCKET_NAME} ${UPX_NAME} ${UPX_PASSWORD}
-./upx info
+echo "start upx upload!"
 ./upx sync ${UPDATE_BLOG_FILE} / -v
+echo "upx upload successful!"
+
 
 
 
