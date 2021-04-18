a0641f57efa5e65b54a293e37397b8658eeba9ed
Forest10 <github.forest10@gmail.com>
2021-04-18 18:54:03 +0800

    $GITHUB_WORKSPACE

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,22 +6,35 @@ cd $GITHUB_WORKSPACE
 ./node_modules/hexo/bin/hexo clean
 #echo "Generate file ..."
 ./node_modules/hexo/bin/hexo generate
-###判断.upx.db存在?
 mkdir -p /github/home/.upx.db
 cd /github/home
+git config --global user.email "github.forest10@gmail.com"
+git config --global user.name "Forest10"
 git clone https://${PERSONAL_TOKEN}@github.com/Forest10/my-hexo-conf.git udx-db
 cd udx-db
 git checkout udx-db
 git pull
-cp /github/home/udx-db/*  /github/home/.upx.db
-cd /github/home/.upx.db && ls
+cp -r $GITHUB_WORKSPACE/public ./
+echo $(date) >date.txt
+git add -A
+git commit -m '哈哈'
+echo 'Start push'
+git push
+echo "push to udx-db-git successful!"
+
+
+
+
+cp -r /github/home/udx-db  /github/home/udx-db_head
+cd /github/home/udx-db_head
+git reset --hard HEAD^
+rsync --dry-run -rcnC --out-format="%n"  /github/home/udx-db/  /github/home/udx-db_head/ |grep -v "/$"|xargs -I{} rsync -R /github/home/udx-db/ ./{} /github/home/diff_out/
+
 
 cd $GITHUB_WORKSPACE/upx-dir/upx-command-dir
 ./upx login ${BUCKET_NAME} ${UPX_NAME} ${UPX_PASSWORD}
 ./upx info
-echo 'find upxDb in sh1' && find ~ -name .upx.db  -d
-./upx sync $GITHUB_WORKSPACE/public / -v
-echo 'find upxDb in sh2' && find ~ -name .upx.db  -d
+./upx sync /github/home/diff_out / -v
 
 
 
