4042f237b80f12f1bc5ceaf808cee81c2709aefb
Forest10 <github.forest10@gmail.com>
2021-04-18 15:06:16 +0800

    UPX NAME

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ cd $GITHUB_WORKSPACE
 ./node_modules/hexo/bin/hexo generate
 
 cd upx-dir/upx-command-dir
-./upx login upload ${UPX_PASSWORD}
+./upx login ${BUCKET_NAME} ${UPX_NAME} upload ${UPX_PASSWORD}
 ./upx info
 cd $GITHUB_WORKSPACE
 upx-dir/upx-command-dir/./upx sync /public / -v
