2f31a8c6ff6d59c0ade49932a33f96307001ff06
Forest10 <github.forest10@gmail.com>
2021-04-18 18:10:03 +0800

    25

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,21 @@ cd $GITHUB_WORKSPACE
 #echo "Generate file ..."
 ./node_modules/hexo/bin/hexo generate
 ###判断.upx.db存在?
-mkdir -p  /home/runner/work/_temp/_github_home/.upx.db
-cd /home/runner/work/_temp/_github_home/
-git clone https://${PERSONAL_TOKEN}@github.com/Forest10/my-hexo-conf.git udx-db
-cd udx-db
-git checkout udx-db
-git pull
-cp /home/runner/work/_temp/_github_home/udx-db/*  /home/runner/work/_temp/_github_home/.upx.db
+#mkdir -p  /home/runner/work/_temp/_github_home/.upx.db
+#cd /home/runner/work/_temp/_github_home/
+#git clone https://${PERSONAL_TOKEN}@github.com/Forest10/my-hexo-conf.git udx-db
+#cd udx-db
+#git checkout udx-db
+#git pull
+#cp /home/runner/work/_temp/_github_home/udx-db/*  /home/runner/work/_temp/_github_home/.upx.db
 
 cd $GITHUB_WORKSPACE/upx-dir/upx-command-dir
 ./upx login ${BUCKET_NAME} ${UPX_NAME} ${UPX_PASSWORD}
 ./upx info
+echo 'find upxDb in sh1' && find ~ -name .upx.db  -d
 ./upx sync $GITHUB_WORKSPACE/public / -v
+echo 'find upxDb in sh2' && find ~ -name .upx.db  -d
+
 
 
 
