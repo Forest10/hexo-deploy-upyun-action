1ada99848fa69724322ecd82523c096448a4b40a
Forest10 <github.forest10@gmail.com>
2021-04-18 18:14:11 +0800

    26

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@ cd $GITHUB_WORKSPACE
 #echo "Generate file ..."
 ./node_modules/hexo/bin/hexo generate
 ###判断.upx.db存在?
-#mkdir -p  /home/runner/work/_temp/_github_home/.upx.db
-#cd /home/runner/work/_temp/_github_home/
-#git clone https://${PERSONAL_TOKEN}@github.com/Forest10/my-hexo-conf.git udx-db
-#cd udx-db
-#git checkout udx-db
-#git pull
-#cp /home/runner/work/_temp/_github_home/udx-db/*  /home/runner/work/_temp/_github_home/.upx.db
+mkdir -p /github/home/.upx.db
+cd /github/home/.upx.db
+git clone https://${PERSONAL_TOKEN}@github.com/Forest10/my-hexo-conf.git udx-db
+cd udx-db
+git checkout udx-db
+git pull
+cp /home/runner/work/_temp/_github_home/udx-db/*  /home/runner/work/_temp/_github_home/.upx.db
 
 cd $GITHUB_WORKSPACE/upx-dir/upx-command-dir
 ./upx login ${BUCKET_NAME} ${UPX_NAME} ${UPX_PASSWORD}
